1c6053c0cce7918c03fc61f17a58618a9d01d175
Nick Sarten <gen.battle@gmail.com>
2015-05-09 13:22:59 +1200

    Implemented tree-building algorithm.
    Still need to implement traversal/search, insertion and deletion.

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,17 @@
 pub mod rkdt {
-    use std::marker::PhantomData;
     pub trait Point {
-        type V: PartialOrd;
+        type V: PartialOrd + Copy;
         // Returns the number of dimensions, k
         fn size(&self) -> usize;
         // For pos = (0 to k-1) returns a value
         fn at(&self, pos: usize) -> Self::V;
     }
     
+    // TODO: Move this out of lib.rs into a separate file
+    #[derive(Clone, Copy)]
     pub struct Vec2 {
-        x: f32,
-        y: f32
+        pub x: f32,
+        pub y: f32
     }
     
     impl Point for Vec2 {
@@ -28,14 +29,14 @@ pub mod rkdt {
             }
         }
     }
-
+    
+    #[derive(Clone, Copy)]
     pub struct Vec3 {
-        x: f32,
-        y: f32,
-        z: f32
+        pub x: f32,
+        pub y: f32,
+        pub z: f32
     }
 
-    // TODO: Implement Vec2
     // TODO: Move this out of lib.rs into a separate file
     impl Point for Vec3 {
         type V = f32;
@@ -65,41 +66,46 @@ pub mod rkdt {
         pub fn new(val: T, l: Option<Box<Node<T>>>, r: Option<Box<Node<T>>>) -> Node<T> {
             Node{value: val, left: l, right: r}
         }
+        
+        pub fn value(&self) -> &T {
+            &self.value
+        }
         // TODO: implement search, insert and delete
         // TODO: add special root node type? (maybe using newtype pattern)
     }
     
     /*
-    // Here's the python code for what this method should do:
-    def kdtree(point_list, depth=0):
-        try:
-            k = len(point_list[0]) # assumes all points have the same dimension
-        except IndexError as e: # if not point_list:
-            return None
-        # Select axis based on depth so that axis cycles through all valid values
-        axis = depth % k
-     
-        # Sort point list and choose median as pivot element
-        point_list.sort(key=itemgetter(axis))
-        median = len(point_list) // 2 # choose median
-     
-        # Create node and construct subtrees
-        return Node(
-            location=point_list[median],
-            left_child=kdtree(point_list[:median], depth + 1),
-            right_child=kdtree(point_list[median + 1:], depth + 1)
-        )
+    Build a k-d tree from a slice of points. Algorithm taken from:
+    http://en.wikipedia.org/wiki/K-d_tree
     */
-    //TODO: sort floats with sort_by(), instead of sort()
-    pub fn from_slice<T: Point + Clone>(points: &[T]) -> Node<T> {
-        // TODO: implement creation of KDTree from slice
-        return Node{value: points[0].clone(), left: None, right: None};
+    pub fn from_slice<T: Point + Copy>(points: &mut [T]) -> Option<Box<Node<T>>> {
+        create_node(points, 0)
+    }
+    
+    fn create_node<T: Point + Copy>(points: &mut [T], depth: usize) -> Option<Box<Node<T>>> {
+        if points.is_empty() {
+            return None;
+        }
+        let k = points[0].size();
+        let axis = depth % k;
+        // This code will panic if the point values are floating point NaN or another incomparable value
+        points.sort_by(|a, b| {
+            a.at(axis)
+            .partial_cmp(&b.at(axis))
+            .unwrap()
+        });
+        let median_index = points.len() / 2;
+        Some(Box::new(Node {
+            value: points[median_index],
+            left: create_node(&mut points[..median_index], depth + 1),
+            right: create_node(&mut points[(median_index + 1)..], depth + 1)
+        }))
     }
 }
 
-
 #[test]
 fn it_works() {
-    let v = vec!(Vec2{x: 3.0, y: 1.0}, Vec2{x: 3.0, y: 1.0}, Vec2{x: 3.0, y: 1.0}, Vec2{x: 3.0, y: 1.0}, Vec2{x: 3.0, y: 1.0}, Vec2{x: 3.0, y: 1.0});
-    let kdt = rkdt::from_slice(v[..]);
+    use rkdt::Vec2;
+    let mut v = vec!(Vec2{x: 3.0, y: 1.0}, Vec2{x: 3.0, y: 1.0}, Vec2{x: 3.0, y: 1.0}, Vec2{x: 3.0, y: 1.0}, Vec2{x: 3.0, y: 1.0}, Vec2{x: 3.0, y: 1.0});
+    let kdt = rkdt::from_slice(&mut v[..]);
 }
