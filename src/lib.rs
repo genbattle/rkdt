690755ff7499917f7aa7158033c987d1ebf37929
Nick Sarten <gen.battle@gmail.com>
2015-05-11 21:51:09 +1200

    Added insert method.
    Being a tidy kiwi, cleaning up the trait requirements too.

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 pub mod rkdt {
     pub trait Point {
-        type V: PartialOrd + Copy;
+        type V: PartialOrd + PartialEq + Copy;
         // Returns the number of dimensions, k
         fn size(&self) -> usize;
         // For pos = (0 to k-1) returns a value
@@ -30,6 +30,12 @@ pub mod rkdt {
         }
     }
     
+    impl PartialEq for Vec2 {
+        fn eq(&self, other: &Self) -> bool {
+            self.x == other.x && self.y == other.y
+        }
+    }
+    
     #[derive(Clone, Copy)]
     pub struct Vec3 {
         pub x: f32,
@@ -54,15 +60,21 @@ pub mod rkdt {
             }
         }
     }
+    
+    impl PartialEq for Vec3 {
+        fn eq(&self, other: &Self) -> bool {
+            self.x == other.x && self.y == other.y && self.z == other.z
+        }
+    }
 
     // TODO: Try to eliminate Boxes by introducing more static parmeters as in from_slice()?
-    pub struct Node<T: Point> {
+    pub struct Node<T: Point + PartialEq + Clone> {
         value: T,
         left: Option<Box<Node<T>>>,
         right: Option<Box<Node<T>>>
     }
 
-    impl<T: Point> Node<T> {
+    impl<T: Point + PartialEq + Clone> Node<T> {
         pub fn new(val: T, l: Option<Box<Node<T>>>, r: Option<Box<Node<T>>>) -> Node<T> {
             Node{value: val, left: l, right: r}
         }
@@ -70,19 +82,41 @@ pub mod rkdt {
         pub fn value(&self) -> &T {
             &self.value
         }
-        // TODO: implement search, insert and delete
+        
+        // insert a new value into the tree
+        pub fn insert(&mut self, value: &T) {
+            self.insert_node(value, 0, value.size());
+        }
+        
+        fn insert_node(&mut self, value: &T, axis: usize, k: usize) {
+            if value.at(axis) < self.value.at(axis) {
+                match self.left {
+                    Some(ref mut n) => n.insert_node(value, (axis + 1) % k, k),
+                    None => self.left = Some(Box::new(Node{value: value.clone(), left: None, right: None}))
+                }
+            } else if value.at(axis) > self.value.at(axis) {
+                match self.right {
+                    Some(ref mut n) => n.insert_node(value, (axis + 1) % k, k),
+                    None => self.right = Some(Box::new(Node{value: value.clone(), left: None, right: None}))
+                }
+            }
+        }
+        // TODO: implement range search
+        // TODO: implement k-nearest-neighbour search
+        // TODO: implement search and delete
         // TODO: add special root node type? (maybe using newtype pattern)
+        // TODO: implement kd-tree-based kmeans
     }
     
     /*
     Build a k-d tree from a slice of points. Algorithm taken from:
     http://en.wikipedia.org/wiki/K-d_tree
     */
-    pub fn from_slice<T: Point + Copy>(points: &mut [T]) -> Option<Box<Node<T>>> {
-        create_node(points, 0)
+    pub fn from_slice<T: Point + PartialEq + Clone>(points: &mut [T]) -> Option<Box<Node<T>>> {
+        create_nodes(points, 0)
     }
     
-    fn create_node<T: Point + Copy>(points: &mut [T], depth: usize) -> Option<Box<Node<T>>> {
+    fn create_nodes<T: Point + PartialEq + Clone>(points: &mut [T], depth: usize) -> Option<Box<Node<T>>> {
         if points.is_empty() {
             return None;
         }
@@ -96,9 +130,9 @@ pub mod rkdt {
         });
         let median_index = points.len() / 2;
         Some(Box::new(Node {
-            value: points[median_index],
-            left: create_node(&mut points[..median_index], depth + 1),
-            right: create_node(&mut points[(median_index + 1)..], depth + 1)
+            value: points[median_index].clone(),
+            left: create_nodes(&mut points[..median_index], depth + 1),
+            right: create_nodes(&mut points[(median_index + 1)..], depth + 1)
         }))
     }
 }
